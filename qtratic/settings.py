87603ba7a27c85f87a6493ab021bda95034e0bd4
Yash Patel <108470744+yashp5545@users.noreply.github.com>
2025-12-03 10:30:51 +0530

    Modify MongoDB connection string for enhanced settings
    Updated MongoDB connection string to include retryWrites and TLS options.

--- qtratic/settings.py
+++ qtratic/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MONGO_CONNECTION_STRING = "mongodb+srv://yashpp5545_db_user:XvPn1Nkv0ok798yt@otratic.oufewvf.mongodb.net/"
+MONGO_CONNECTION_STRING = "mongodb+srv://yashpp5545_db_user:XvPn1Nkv0ok798yt@otratic.oufewvf.mongodb.net/otratic?retryWrites=true&w=majority&tls=true"
 
 # MongoDB Configuration
 # MONGO_CONNECTION_STRING = 'mongodb+srv://yashpp5545_db_user:XvPn1Nkv0ok798yt@otratic.oufewvf.mongodb.net/admin?retryWrites=true&loadBalanced=false&replicaSet=atlas-jvka71-shard-0&readPreference=primary&srvServiceName=mongodb&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1'
